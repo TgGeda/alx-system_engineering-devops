c329107c8be276183ade66f7cca2e492c4f59f58
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-03 13:34:14 +0300

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,1 +1,12 @@
 
+# Define the regular expression
+pattern = /School/
+
+# Get the first argument
+input = ARGV[0]
+
+# Check if the input matches the pattern
+result = pattern =~ input
+
+# Print the result
+puts result
